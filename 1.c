97eeb86263f58622fbaa88ffaaa4953df2539f26
Chinaski <emindanew@yandex.ru>
2017-02-19 16:40:07 +0800

    Implementation of test functions and term deposit

--- 1.c
+++ 1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+
+int main(){
+	int sum,time;
+	printf("deposit Amount\n");
+	scanf("%d",&sum);
+	printf("For how long ?\n");
+	scanf("%d",&time);
+	if ( sum < 1000 && time > 365 ) {
+		printf("The minimum deposit amount in 1000\nThe term does not exceed 365 days\n");
+		return 0;
+	}
+	if (sum < 1000 ) {
+		printf("The minimum deposit amount in 1000\n");
+		return 0;
+	}
+	if (time > 365) {
+		printf("The term does not exceed 365 days\n");
+		return 0;
+	}
+
+	return 0;
+}
