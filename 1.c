79c36a2c597c5e071264f5b79a573db572676a25
Chinaski <emindanew@yandex.ru>
2017-02-19 17:12:16 +0800

    Implementation of functions for the deposit amount> 100 tr

--- 1.c
+++ 1.c
@@ -32,6 +32,17 @@ int main(){
 
 
 	}
+	else {
+         if (time < 31 && time > 0)
+            sum*=0.9;
+        else if (time < 121 && time > 30)
+            sum*=1.03;
+
+        else if (time < 241 && time > 120)
+            sum*=1.08;
+        else
+            sum*=1.15;
+	}
 	printf("%d\n",sum);
 
 	return 0;
